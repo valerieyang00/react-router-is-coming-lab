954614204492cde32491549435d8a3657bc6c9aa
valerieyang00 <valerieyang00@gmail.com>
2022-09-30 16:13:34 -0700

    not using Nav

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import House from './components/House'
 import Houses from './components/Houses'
 import Member from './components/Member'
-import Nav from './partials/Nav'
+// import Nav from './partials/Nav'
 import gameOfThrones from './gameOfThrones'
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Nav />
     <Routes>
-      <Route path='/houses' element={<Houses houses={gameOfThrones} />} />
+      <Route path='/' element={<Houses houses={gameOfThrones} />} />
       <Route path='/houses/:id' element={<House houses={gameOfThrones}/>} />
       <Route path='/houses/:houseId/members/:memberId' element={<Member houses={gameOfThrones} />} />      
     </Routes>
